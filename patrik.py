ffa00ac474ef6c479882bfd1126d1aeac3146b29
Alonso <alonso@losmolina.org>
2026-07-18 17:17:36 -0600

    First funcitonal version of the app. Excercise data added and implementes note saving and loading funcitonality.

--- patrik.py
+++ patrik.py
@@ -1,9 +1,12 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, asdict
+import json
 from textual.app import App, ComposeResult
-from textual.widgets import Footer, Header, Collapsible, TextArea, TabbedContent, TabPane, Label
+from textual.widgets import Footer, Header, Collapsible, TextArea, TabbedContent, TabPane, Label, Rule
+from textual.containers import VerticalGroup
+from textual.events import Resize
 
-
-app_title = "     _/\/\/\/\/\__________________/\/\__________________/\/\____/\/\_______\n\
+app_title = "\
+     _/\/\/\/\/\__________________/\/\__________________/\/\____/\/\_______\n\
     _/\/\____/\/\__/\/\/\______/\/\/\/\/\__/\/\__/\/\__________/\/\__/\/\_  \n\
    _/\/\/\/\/\________/\/\______/\/\______/\/\/\/\____/\/\____/\/\/\/\___   \n\
   _/\/\__________/\/\/\/\______/\/\______/\/\________/\/\____/\/\/\/\___    \n\
@@ -30,42 +33,79 @@ class Excercise:
     description: str
     notes: str
 
-chop_builder = Excercise("Chop Builder", 10, "desc", None)
-notas = Excercise("Encontrar notas", 5, None, "")
 
-v1 = [chop_builder, notas]
+class ExcerciseDisplay(VerticalGroup):
 
+    def __init__(self, excercise: Excercise, offset: int):
+        super().__init__()
+        self.excercise = excercise
+        self.whitespace_offset = offset
+    
+    def compose(self):
+        #whitespace = self.whitespace_offset - len(self.excercise.name) - len(str(self.excercise.time))
+        #title = self.excercise.name + " "*whitespace + str(self.excercise.time) + " min"
+        with Collapsible(classes="excercise"):
+            ta = TextArea(classes="notes", text=self.excercise.notes)
+            ta.border_title = "Notas"
+            yield ta
+
+    def on_text_area_changed(self, event: TextArea.Changed):
+        self.excercise.notes = event.text_area.text
+        self.app.save_data() #Is this a terrile way to do this?
+
+    def on_collapsible_expanded(self, event):
+        tab_pane = self.parent
+        for coll in tab_pane.query("ExcerciseDisplay Collapsible"):
+            if coll != event.collapsible:
+                coll.collapsed = True
+
+    def on_resize(self, event: Resize):
+        self.update_title()
+
+    def update_title(self):
+        coll = self.query_one("Collapsible")
+        whitespace = self.size.width - len(self.excercise.name) - len(str(self.excercise.time)) - 7
+        coll.title = self.excercise.name + " "*whitespace + str(self.excercise.time) + " min"
 
 
 class PatrickApp(App):
     """Musical practice aid for the terminal"""
     
     CSS_PATH = "patrik.tcss"
+    sessions = []
 
     def compose(self) -> ComposeResult:
+        self.load_data()
+
         yield Header()
-
         yield Label(app_title_2, classes="app_title")
-
         with TabbedContent():
-            with TabPane("v1"):
-
-                for ex in v1:
-                    with Collapsible(title=ex.name):
-                        if ex.description:
-                            yield Label(ex.description)
-                        ta = TextArea(classes="notes")
-                        ta.border_title = "Notas"
-                        yield ta
-
-            with TabPane("v2"):
-                yield Label("v2")
-
+            for i, session in enumerate(self.sessions):
+                with TabPane(f"v{i+1}"):
+                    max_name_len = max([len(ex.name) for ex in session])
+                    offset = max_name_len + 30
+                    for excercise in session:
+                        yield ExcerciseDisplay(excercise, offset)
         yield Footer()
 
 
     def on_mount(self):
         self.animation_level = "none"
+
+    def load_data(self):
+        try:
+            with open("data.json", "r") as file:
+                self.sessions = [[Excercise(**x) for x in s] for s in json.load(file)]
+        except:
+            self.sessions = [[]]
+
+
+    def save_data(self):
+        data = [[asdict(x) for x in s ] for s in self.sessions]
+        with open("data.json", "w") as file:
+            json.dump(data, file, indent = 4)
+
+
 
 if __name__ == "__main__":
     app = PatrickApp()
